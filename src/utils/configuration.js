851740cf3a63e035135c18255d2a4ad43399c53d
chenmau1128 <156451538+chenmau1128@users.noreply.github.com>
2024-01-13 00:24:15 +0800

    BM

--- src/utils/configuration.js
+++ src/utils/configuration.js
@@ -1,17 +1,17 @@
 // Set smart contract based on chain id
 export const CONTRACTADDRESS = {
-    1: "",
-    4: "",
+    1: "0xdcAE5cB325F11CF1b7F49B155391d14Dd98eE327",
+    5: "",
 };
 
 // Set the price of your NFT
-export const PRICE = 0;
+export const PRICE = 0.02;
 
 // Set the max amount of your NFT collection
-export const MAX_AMOUNT = 1000;
+export const MAX_AMOUNT = 1200;
 
 // Set the max batch size per wallet 
-export const MAX_BATCH_SIZE = 9;
+export const MAX_BATCH_SIZE = 100;
 
 // Set the opesnea url 
 export const OPENSEA_URL = "https://opensea.io/collection/"
